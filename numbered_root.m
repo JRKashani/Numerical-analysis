a9eb2ae4469f1f57ad1810a48f756fc8156997d3
JRKashani <72232619+JRKashani@users.noreply.github.com>
2025-11-28 00:06:25 +0200

    Update numbered_root.m

--- numbered_root.m
+++ numbered_root.m
@@ -10,7 +10,8 @@ function [root_value] = numbered_root(Bi, m)
         vector_of_J0_roots = readmatrix("ROOTS.txt");
     end
 
-    
+    upper_boundary = vector_of_J0_roots(m) - 0.1;
+    lower_boundary = vector_of_J0_roots(m-1) - 0.1
     
 
 
