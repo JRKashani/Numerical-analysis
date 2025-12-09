9d3e6f72813059684974169b8881f84652e9ae8f
JRKashani <72232619+JRKashani@users.noreply.github.com>
2025-12-09 20:35:21 +0200

    new project
    will give a heat map of a metal board

---
+++ second_project/SetGlobal.m
@@ -0,0 +1,9 @@
+function [~] = SetGlobal(~)
+
+    global highest_temp, lowest_temp, mat_size_1, mat_size_2 
+    highest_temp = 100;
+    lowest_temp = -30;
+    mat_size_1 = 16;
+    mat_size_2 = mat_size_1;
+
+end
