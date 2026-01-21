132c328cbf143ca4969091e1e8b1453e036fdbab
JRKashani <72232619+JRKashani@users.noreply.github.com>
2026-01-21 17:20:34 +0200

    Add Blasius boundary layer equation function stub
    Introduces a new MATLAB function 'Blasius_Boundary_layer_equation' that sets up the interface and documentation for solving the Blasius boundary layer equation. The function currently contains placeholder variables and comments describing its intended inputs and outputs.

---
+++ third project/Blasius_Boundary_layer_equation.m
@@ -0,0 +1,8 @@
+function [eta, y, y_der, y_sec_der] = Blasius_Boundary_layer_equation(sec_der_of_f_at_0)
+% The function will accept an initial value for the second derivitive of
+% the funncion "f" at [eta = 0] and return 4 vectors - values of eta and
+% corresponding values of the function and its derivitives
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
